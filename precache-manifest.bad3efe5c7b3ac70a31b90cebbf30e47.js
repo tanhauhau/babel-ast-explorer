0fb527357d61129a0294d0de290752dd59996fa3
Tan Li Hau <lhtan93@gmail.com>
2019-05-23 23:58:22 +0800

    Built on Thu May 23 2019 23:58:22 GMT+0800 (+08)

---
+++ precache-manifest.bad3efe5c7b3ac70a31b90cebbf30e47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b708baf5b1ac0d7fd014",
+    "url": "/babel-ast-explorer/static/js/runtime~main.8923bb34.js"
+  },
+  {
+    "revision": "7766a0b4882dc52cfb86",
+    "url": "/babel-ast-explorer/static/js/main.0b2f6a55.chunk.js"
+  },
+  {
+    "revision": "7b74fc0cbc191b4771d1",
+    "url": "/babel-ast-explorer/static/js/2.a9d25c29.chunk.js"
+  },
+  {
+    "revision": "7766a0b4882dc52cfb86",
+    "url": "/babel-ast-explorer/static/css/main.070925da.chunk.css"
+  },
+  {
+    "revision": "7b74fc0cbc191b4771d1",
+    "url": "/babel-ast-explorer/static/css/2.487dca0a.chunk.css"
+  },
+  {
+    "revision": "1aa45cfc40f383110dfd55c0fe525b67",
+    "url": "/babel-ast-explorer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/babel-ast-explorer/precache-manifest.e4f27e3409e62d4585531f1ddac4b718.js"
+  "/babel-ast-explorer/precache-manifest.bad3efe5c7b3ac70a31b90cebbf30e47.js"
 );
 
 workbox.clientsClaim();
